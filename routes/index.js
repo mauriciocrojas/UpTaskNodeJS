45d6478fbe0dfde5a2d85092572b20c99f9a3cbc
MauricioCRojas <mauriciocc.rojas@gmail.com>
2022-01-15 19:01:37 -0300

    agregamos rutas

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const proyectosController = require
 ("../controllers/proyectosController");
 
 module.exports = function(){
-
     //ruta para el home
     router.get("/", proyectosController.proyectosHome);
     router.get("/nosotros", proyectosController.nosotros);
