254156401bd11f9fe6cc1dab145462c502b8581e
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-02-17 23:12:40 -0300

    completamos barra de porcentaje de tareas

--- public/js/funciones/avance.js
+++ public/js/funciones/avance.js
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 export const actualizarAvance = () => {
     //seleccionar las tareas existentes
     const tareas = document.querySelectorAll("li.tarea");
@@ -14,5 +16,12 @@ export const actualizarAvance = () => {
         const porcentaje = document.querySelector("#porcentaje");
         porcentaje.style.width = avance + "%";
 
+        if(avance === 100){
+            Swal.fire(
+                "Completaste el Proyecto",
+                "Felicidades, has terminado tus tareas",
+                "success"
+            );
+        }
     }
 }
