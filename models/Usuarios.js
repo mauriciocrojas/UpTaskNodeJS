f707eb859a6d06b07bc1a770f3373b3b6043b0e5
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-02-18 22:04:38 -0300

    hasheamos los passwords

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 const db = require("../config/db");
 const Proyectos = require("../models/Proyectos");
+const bcrypt = require("bcrypt-nodejs");
 
 const Usuarios = db.define("usuarios", { 
     id: {
@@ -15,6 +16,12 @@ const Usuarios = db.define("usuarios", {
     password: {
         type: Sequelize.STRING(60),
         allowNull: false
+    }
+}, {
+    hooks: {
+        beforeCreate(usuario) {
+            usuario.password = bcrypt.hashSync(usuario.password, bcrypt.genSaltSync(10));
+        }
     }
 });
 
